2dbcd158e836171e9136af9d35771d14e9c1ac93
kantegory <davidobryakov@gmail.com>
2020-05-24 22:27:37 +0300

    Add HelloHandler && testify

--- Makefile
+++ Makefile
@@ -3,4 +3,8 @@
 build:
 	go build -v ./cmd/api-server
 
+.PHONY: test
+test:
+	go test -v -race -timeout 30s ./...
+
 .DEFAULT_GOAL := build
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,12 +2,16 @@ package apiserver
 
 import (
 	"github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io"
 )
 
 // APIServer ...
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New ...
@@ -15,6 +19,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -24,9 +29,10 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter()
 	s.logger.Info("Starting API server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 // configureLogger ...
@@ -40,4 +46,16 @@ func (s *APIServer) configureLogger() error {
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+// configureRouter ...
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+// handleHello ...
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
