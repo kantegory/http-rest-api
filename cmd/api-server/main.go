f85cf84588e95b141b6993721ca30399010276af
kantegory <davidobryakov@gmail.com>
2020-05-24 22:07:17 +0300

    Add configs, set up logrus && toml

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/BurntSushi/toml"
+	"log"
+	"flag"
+	"github.com/kantegory/http-rest-api/internal/app/apiserver"
+)
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "conf/apiserver.toml", "Path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kantegory/http-rest-api
 
 go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.6.0
+)
